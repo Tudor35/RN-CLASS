f297420c879d55d744f714c8c5a454f5b82822e7
Tudor35 <Tudor35@github.com>
2024-06-03 19:14:02 +0000

    update

--- App.js
+++ App.js
@@ -3,13 +3,30 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View style={{flex:1 , padding: 35, backgroundColor: "red"}}>
+    <View style={{flex:1 , padding: 35, backgroundColor: "white", gap: 10}}>
       <View style={{display: "flex", flexDirection: "row", gap: 10, justifyContent: "center"}}>
         <View>
 
           <View style = {{backgroundColor: "blue", height: 200, width: 175}}>
             <Text>
-              Hello THeo
+              Hello Theo
+            </Text>
+
+          </View>
+          
+        </View>
+
+        <View style = {{backgroundColor: "gold", height: 200, width: 175 }}>
+          <Text>Hello Eben</Text>
+        </View>
+      </View>
+
+      <View style={{display: "flex", flexDirection: "row", gap: 10, justifyContent: "center"}}>
+        <View>
+
+          <View style = {{backgroundColor: "blue", height: 200, width: 175}}>
+            <Text>
+              Hello Theo
             </Text>
 
           </View>
@@ -22,7 +39,8 @@ const App = () => {
       </View>
     </View>
   
-  )
+    )
+    
 }
 
 export default App
